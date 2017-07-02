54fdfc7765346587937d719056b1f411d2ac79b9
simran Kashyap <simrankashyap907@gmail.com>
2017-07-03 00:21:44 +0530

    A simple calculator

---
+++ Calc.r
@@ -0,0 +1,8 @@
+#Scientific calc
+print("enter your first number")
+a=scan()
+print("enter your second number")
+b=scan()
+print("enter the operator")
+c=scan(what='character')
+print(switch(c,"+"=a+b,"-"=a-b,"*"=a*b))
