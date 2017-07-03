18edb36fdf51ace7f75f82f5d42bf6af1aac3306
simran Kashyap <simrankashyap907@gmail.com>
2017-07-03 19:49:27 +0530

    To find leap year

---
+++ leapyear.R
@@ -0,0 +1,18 @@
+leapyear <- function(year)
+{
+  if(year%%4==0)
+  {
+    
+    if((year%%100)==0)
+    { if((year%%400)==0)
+    { print("leap year")}
+      else{
+        print("Not a leap year")
+      }
+    }else{
+      print("leap year")
+    }
+  }else{
+    print("Not a leap year")
+  }
+}
